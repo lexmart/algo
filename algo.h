57434e8500279d9c7b512e85341568a77c868811
alexander martin <alexandermartin006@gmail.com>
2016-07-05 16:30:57 -0700

    binary search trees

--- algo.h
+++ algo.h
@@ -5,6 +5,7 @@
 #include <stdint.h>
 #include <limits.h>
 #include <stdbool.h>
+#include <math.h>
 
 typedef uint8_t u8;
 typedef uint16_t u16;
@@ -20,6 +21,14 @@ typedef float r32;
 typedef double r64;
 
 typedef int b32;
+
+typedef struct node_t
+{
+    int value;
+    struct node_t *left;
+    struct node_t *right;
+    struct node_t *parent;
+} node_t;
 
 #define inline __inline
 
@@ -77,6 +86,97 @@ inline int random_int(int min, int max)
     int result = min + offset;
     assert((result >= min) && (result <= max));
     return result;
+}
+
+void print_tree(node_t *node)
+{
+    node_t *tree[256];
+    
+    tree[0] = node;
+    int leaves_start_index = 0;
+    int leaves_count = 1;
+    
+    while(1)
+    {
+        b32 has_non_null_leafs = false;
+        
+        int new_leaves_start_index = leaves_start_index + leaves_count;
+        for(int i = 0; i < leaves_count; i++)
+        {
+            node_t *leaf = tree[leaves_start_index + i];
+            node_t **leaf_left = &tree[new_leaves_start_index + 2*i];
+            node_t **leaf_right = &tree[new_leaves_start_index + 2*i + 1];
+            
+            if(leaf)
+            {
+                has_non_null_leafs = true;
+                *leaf_left = leaf->left;
+                *leaf_right = leaf->right;
+            }
+            else
+            {
+                *leaf_left = 0;
+                *leaf_right = 0;
+            }
+        }
+        
+        leaves_start_index = new_leaves_start_index;
+        leaves_count *= 2;
+        
+        if(!has_non_null_leafs)
+        {
+            break;
+        }
+    }
+    
+    int num_nodes = leaves_start_index;
+
+    
+    /* This code for printing tree was obtained from
+    http://stackoverflow.com/questions/801740/c-how-to-draw-a-binary-tree-to-the-console */
+#define PARENT(i) ((i-1) / 2)
+#define LINE_WIDTH 128
+    
+    int *print_pos = malloc(num_nodes*sizeof(int));
+    int i, j, k, pos, x=1, level=0;
+    
+    print_pos[0] = 0;
+    for(i = 0,j = 1; i < num_nodes; i++,j++) {
+        pos = print_pos[PARENT(i)] + (i%2?-1:1)*(LINE_WIDTH/(pow(2,level+1))+1);
+        
+        for (k=0; k<pos-x; k++) printf("%c",i==0||i%2?' ':'=');
+        if(tree[i]) printf("%d",tree[i]->value);
+        else printf("=");
+        
+        print_pos[i] = x = pos+1;
+        if (j==pow(2,level)) {
+            printf("\n");
+            level++;
+            x = 1;
+            j = 0;
+        }
+    }
+    
+    free(print_pos);
+}
+
+void int_to_string(int value, char *output, int output_count)
+{
+    int i = 0;
+    while(value > 0)
+    {
+        assert(i < output_count);
+        output[i++] = (value % 10) + '0';
+        value /= 10;
+    }
+    
+    for(int j = 0; j < (i / 2); j++)
+    {
+        int k = i - j - 1;
+        int temp = output[k];
+        output[k] = output[j];
+        output[j] = temp;
+    }
 }
 
 #define ALGO_H
---
+++ binary_search_trees.c
@@ -0,0 +1,250 @@
+#include "algo.h"
+
+typedef struct bst_t
+{
+    node_t *root;
+} bst_t;
+
+
+bst_t create_bst()
+{
+    bst_t result;
+    result.root = 0;
+    return result;
+}
+
+void free_tree(node_t *node)
+{
+    if(node)
+    {
+        free_tree(node->left);
+        free_tree(node->right);
+        free(node);
+    }
+}
+
+void verify_children_linked_to_parents(node_t *node)
+{
+    if(node)
+    {
+        if(node->left) assert(node->left->parent == node);
+        if(node->right) assert(node->right->parent == node);
+        verify_children_linked_to_parents(node->left);
+        verify_children_linked_to_parents(node->right);
+    }
+}
+
+/* Assuming all keys are distinct */
+node_t *search(bst_t *tree, int value)
+{
+    node_t *current = tree->root;
+    
+    while(current && current->value != value)
+    {
+        if(value <= current->value)
+        {
+            current = current->left;
+        }
+        else
+        {
+            current = current->right;
+        }
+    }
+    
+    return current;
+}
+
+node_t *minimum(node_t *sub_tree)
+{
+    assert(sub_tree);
+    while(sub_tree->left)
+    {
+        sub_tree = sub_tree->left;
+    }
+    return sub_tree;
+}
+
+node_t *maximum(node_t *sub_tree)
+{
+    assert(sub_tree);
+    while(sub_tree->right)
+    {
+        sub_tree = sub_tree->right;
+    }
+    return sub_tree;
+}
+
+node_t *successor_from_value(bst_t *tree, int value)
+{
+    node_t *result = 0;
+    
+    node_t *node = search(tree, value);
+    if(node->right)
+    {
+        result = minimum(node->right);
+    }
+    else
+    {
+        while(node->parent && node->parent->right == node)
+        {
+            node = node->parent;
+        }
+        result = node->parent;
+    }
+    
+    return result;
+}
+
+node_t *successor_from_node(bst_t *tree, node_t *node)
+{
+    node_t *result = 0;
+    
+    if(node->right)
+    {
+        result = minimum(node->right);
+    }
+    else
+    {
+        while(node->parent && node->parent->right == node)
+        {
+            node = node->parent;
+        }
+        result = node->parent;
+    }
+    
+    return result;    
+}
+
+void insert(bst_t *tree, int value)
+{
+    assert(search(tree, value) == 0); // avoid duplicates
+    
+    node_t *parent = 0;
+    node_t *current = tree->root;
+    
+    while(current != 0)
+    {
+        parent = current;
+        if(value <= current->value) current = current->left;
+        else current = current->right;
+    }
+    
+    node_t *new_node = malloc(sizeof(node_t));
+    new_node->value = value;
+    new_node->left = 0;
+    new_node->right = 0;
+    
+    if(parent == 0)
+    {
+        tree->root = new_node;
+    }
+    else if(value <= parent->value)
+    {
+        parent->left = new_node;
+    }
+    else
+    {
+        parent->right = new_node;
+    }
+    
+    new_node->parent = parent;
+}
+
+void transplant(bst_t *tree, node_t *dest_tree, node_t *source_tree)
+{
+    if(dest_tree->parent == 0)
+    {
+        tree->root = source_tree;
+    }
+    else if(dest_tree == dest_tree->parent->left)
+    {
+        dest_tree->parent->left = source_tree;
+    }
+    else
+    {
+        dest_tree->parent->right = source_tree;
+    }
+    
+    if(source_tree)
+    {
+        source_tree->parent = dest_tree->parent;
+    }
+}
+
+void delete(bst_t *tree, node_t *deleted_node)
+{
+    assert(deleted_node);
+    
+    if(deleted_node->left == 0)
+    {
+        transplant(tree, deleted_node, deleted_node->right);
+    }
+    else if(deleted_node->right == 0)
+    {
+        transplant(tree, deleted_node, deleted_node->left);
+    }
+    else
+    {
+        node_t *replacement_node = minimum(deleted_node->right);
+        
+        if(replacement_node->parent != deleted_node)
+        {
+            transplant(tree, replacement_node, replacement_node->right);
+            replacement_node->right = deleted_node->right;
+            deleted_node->right->parent = replacement_node;
+        }
+        
+        replacement_node->left = deleted_node->left;
+        deleted_node->left->parent = replacement_node;
+        replacement_node->parent = deleted_node->parent;
+        transplant(tree, deleted_node, replacement_node);
+        
+    }
+}
+
+/* assumes array has all distinct integers */
+void binary_search_tree_sort(int *array, int n)
+{
+    bst_t bst = create_bst();
+    for(int i = 0; i < n; i++)
+    {
+        insert(&bst, array[i]);
+    }
+    
+    node_t *current_node = minimum(bst.root);    
+    for(int i = 0; i < n; i++)
+    {
+        array[i] = current_node->value;
+        current_node = successor_from_node(&bst, current_node);
+    }
+    
+    free_tree(bst.root);
+}
+
+int main()
+{
+    int array[10] = {7, 3, 1, 8, 4, 9, -7, 23, -5, 2};
+    binary_search_tree_sort(array, array_count(array));
+    print_array(array);
+    
+    bst_t bst = create_bst();
+
+    insert(&bst, 1);
+    insert(&bst, 6);
+    insert(&bst, 3);
+    insert(&bst, 9);
+    insert(&bst, 0);
+    insert(&bst, 10);
+    insert(&bst, 2);
+    insert(&bst, 7);
+    insert(&bst, 5);
+    insert(&bst, 8);
+    insert(&bst, -4);
+    insert(&bst, -2);
+
+    
+    print_tree(bst.root);
+    delete(&bst, search(&bst, 6));
+    print_tree(bst.root);
+    getchar();
+}
